63971df7f357f407642a08d6f88d86c86f3cfe2e
Dree_√√ <160780819+Andre-ognibene@users.noreply.github.com>
2024-10-14 15:46:25 -0300

    Delete efeito.js

--- efeito.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = function() {
-    var scrollPosition = window.scrollY;
-    var windowHeight = window.innerHeight;
-    var opacity = Math.min(scrollPosition / windowHeight, 0.7); // Máxima opacidade de 0.7
-    document.querySelector('.overlay').style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-};
